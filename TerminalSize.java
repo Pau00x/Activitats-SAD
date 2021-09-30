1ebaf58197063294f26bf71dbe19fc478b0920ce
Pau Villaverde Magrí <pauvima15@gmail.com>
2021-09-30 14:41:34 +0200

    Add files via upload

---
+++ TerminalSize.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+/**
+ *
+ * @author pau
+ */
+public class TerminalSize {
+    
+    public void getCols() {
+        String[] cmd = {"/bin/sh", "-c", "tput cols"};
+        try {
+            Process pr = Runtime.getRuntime().exec(cmd);
+            pr.waitFor();
+            BufferedReader br = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+            String line;
+            while((line=br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        TerminalSize t = new TerminalSize();
+        t.getCols();
+    }
+}
